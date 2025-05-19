274a17114d9c11bfccc46d2c0efd0c237d28eca1
Sipepper <valera1gribnik@gmail.com>
2025-05-20 02:14:22 +0300

    added some snippets and testing pictures in fetch

--- assets/snippets/all.lua
+++ assets/snippets/all.lua
@@ -1,10 +1,5 @@
 return {
-  s("trig", t("loaded!!")),
-  s("fr", t("\fraction{}{}")),
-  s("pma", t([[
-    \begin{pmatrix}
-      _ 
-    \end{pmatrix}
-    ]]
-  ))
+  s("fr", t("\\fraction{"),i(0),t("}{"),i(1),t("}")),
+  s("pma", { t("\\begin{pmatrix}"), i(0), t("\\end{pmatrix}") }),
+  s("pd", { t("\\frac{\\partial "),i(0),t("}{\\partial "),i(1),t("}") }),
 }
