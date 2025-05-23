24d33f3d3552109e86dccb73fa8275bb474629b9
Sipepper <valera1gribnik@gmail.com>
2025-05-24 01:04:46 +0300

    .

--- assets/yazi_init.lua
+++ assets/yazi_init.lua
@@ -23,23 +23,23 @@ table.insert(bookmarks, {
 })
 
 
-require("yamb"):setup({
-  -- Optional, the path ending with path seperator represents folder.
-  bookmarks = bookmarks,
-  -- Optional, recieve notification everytime you jump.
-  jump_notify = true,
-  -- Optional, the cli of fzf.
-  cli = "fzf",
-  -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
-  keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  -- Optional, the path of bookmarks
-  path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or
-    (os.getenv("HOME") .. "/.config/yazi/bookmark"),
-})
+-- require("yamb"):setup({
+--   -- Optional, the path ending with path seperator represents folder.
+--   bookmarks = bookmarks,
+--   -- Optional, recieve notification everytime you jump.
+--   jump_notify = true,
+--   -- Optional, the cli of fzf.
+--   cli = "fzf",
+--   -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+--   keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+--   -- Optional, the path of bookmarks
+--   -- path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+--   path = "home/mlys/.config/yazi/bookmark",
+-- })
 
 require("full-border"):setup()
 
--- require("relative-motions"):setup({ show_numbers="relative", show_motion = true, enter_mode ="first" })
+require("relative-motions"):setup({ show_numbers="relative", show_motion = true, enter_mode ="first" })
 
 require("yatline"):setup({
   section_separator = { open = "", close = "" },
