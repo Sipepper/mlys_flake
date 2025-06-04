e867519c6790126f14e4fb6925bd13627190dd84
Sipepper <valera1gribnik@gmail.com>
2025-06-04 22:05:24 +0300

    trying different scaling on desktop

--- desktop.nix
+++ desktop.nix
@@ -139,7 +139,7 @@
         name = "e-signal-hator-pulsar";
         sensitivity = "-0.5";
       };
-      monitor = if config.default.isPC then ",1920x1080@75,auto,auto" else ",1920x1080,auto,auto";
+      monitor = if config.default.isPC then ",1920x1080@75,auto,1.2" else ",1920x1080,auto,auto";
 
       xwayland.force_zero_scaling = true;
 
