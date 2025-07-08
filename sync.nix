7aa3d8d101976ee2cfd3183c6284fadc20b92941
Sipepper <valera1gribnik@gmail.com>
2025-07-08 18:01:32 +0300

    Sync of all other important folders

--- sync.nix
+++ sync.nix
@@ -9,12 +9,15 @@
     settings = {
       devices = {
         "phone" = { id = "TVV4OKR-JCDYDZQ-3EFMNF2-ZIXBPYO-XRIJVGO-4OCOLAO-S3IQGGK-IXLNPAK"; };
+        "laptop" = { id = "3RXTYVP-BCN2EDP-L5JK4VN-GPMKIMQ-MTURSLG-HSY2N7E-UL4B75X-SZVFUAQ";};
       };
       folders = {
-        "Obsidian" = {
-          path = "/home/mlys/Obsidian";
-          devices = [ "phone" ];
-        };
+        "Obsidian" = { path = "/home/mlys/Obsidian"; devices = [ "phone" ]; };
+        "Work" = { path = "/home/mlys/Work"; devices = [ "phone" ]; };
+        "Rust" = { path = "/home/mlys/Rust"; devices = [ "phone" ]; };
+        "LaTeX" = { path = "/home/mlys/LaTeX"; devices = [ "phone" ]; };
+        "Books" = { path = "/home/mlys/Books"; devices = [ "laptop" ]; };
+        "Git" = { path = "/home/mlys/Git"; devices = [ "phone" ]; };
       };
     };
   };
