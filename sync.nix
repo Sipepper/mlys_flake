4f4a84d95274382205f7e2f3645168fb24eab3ed
Sipepper <valera1gribnik@gmail.com>
2025-07-08 18:14:47 +0300

    laptop related changes

--- sync.nix
+++ sync.nix
@@ -9,14 +9,14 @@
     settings = {
       devices = {
         "phone" = { id = "TVV4OKR-JCDYDZQ-3EFMNF2-ZIXBPYO-XRIJVGO-4OCOLAO-S3IQGGK-IXLNPAK"; };
-        "laptop" = { id = "3RXTYVP-BCN2EDP-L5JK4VN-GPMKIMQ-MTURSLG-HSY2N7E-UL4B75X-SZVFUAQ";};
+        "pc" = { id = "EKSQELD-DFRSA35-C3LCKGA-IBOUVLL-EYGJCRC-ABUV73T-SGFP5OZ-IAAQGAB";};
       };
       folders = {
         "Obsidian" = { path = "/home/mlys/Obsidian"; devices = [ "phone" ]; };
         "Work" = { path = "/home/mlys/Work"; devices = [ "phone" ]; };
         "Rust" = { path = "/home/mlys/Rust"; devices = [ "phone" ]; };
         "LaTeX" = { path = "/home/mlys/LaTeX"; devices = [ "phone" ]; };
-        "Books" = { path = "/home/mlys/Books"; devices = [ "laptop" ]; };
+        "Books" = { path = "/home/mlys/Books"; devices = [ "pc" ]; };
         "Git" = { path = "/home/mlys/Git"; devices = [ "phone" ]; };
       };
     };
