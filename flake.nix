468fea1985e68666ae1d99b1deace5bed60f0944
Sipepper <valera1gribnik@gmail.com>
2025-10-20 00:02:02 +0300

    noctalia added to flake

--- flake.nix
+++ flake.nix
@@ -14,7 +14,15 @@
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    quickshell = {
+      url = "github:outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.quickshell.follows = "quickshell";  # Use same quickshell version
+    };   
   };
 
   outputs = { 
@@ -28,7 +36,8 @@
         modules = [
           ./configuration.nix
           inputs.home-manager.nixosModules.default
-          determinate.nixosModules.default
+            determinate.nixosModules.default
+            ./noctalia.nix
         ];
       };
     };
