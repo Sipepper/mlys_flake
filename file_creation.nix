3babadb5c2abf416498c2440c39af7ec07dd3745
Sipepper <valera1gribnik@gmail.com>
2026-01-09 16:05:26 +0200

    Nvim and yazi desktop file + vencord theme

--- file_creation.nix
+++ file_creation.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, nixpkgs-stable, inputs, lib, ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./default.nix
@@ -41,10 +41,43 @@
         Name=Kega Fusion
       '';
     };
+
+    ".local/share/applications/nvim.desktop" = {
+      text = ''
+        [Desktop Entry]
+        Categories=Utility;TextEditor
+        Comment=Edit text files
+        Exec=nvim
+        GenericName=Text Editor
+        Icon=nvim
+        MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++
+        Name=Neovim
+        StartupNotify=true
+        Terminal=true
+        Type=Application
+        Version=1.5
+      '';
+    };
+
+    ".local/share/applications/yazi.desktop" = {
+      text = ''
+        [Desktop Entry]
+        Type=Application
+        Name=Yazi in Wezterm
+        Comment=Launch Yazi file manager in the Wezterm terminal
+        Exec=wezterm -e yazi
+        Icon=yazi # Or specify the full path to an icon file, e.g., /path/to/yazi-icon.png
+        Terminal=false # Wezterm handles the terminal, so we set this to false
+        Categories=System;Utility;FileManager;
+        MimeType=inode/directory;
+      '';
+    };
+
     ".assets/tex/preamble.tex" = { source = ./assets/tex/preamble.tex; };
     ".assets/tex/general.bib" = { source = ./assets/tex/bib.bib; };
     ".assets/tex/listings-rust.sty" = { source = ./assets/tex/listings-rust.sty; };
     ".assets/nu/pack.nu" = { source = ./assets/scripts/pack.nu; };
+    ".assets/vencord/system24.theme.css" = { source = ./assets/system24.theme.css; };
 
   };
 
