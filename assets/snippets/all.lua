d9788964a4d8ebaf52b4743c8db48a86a02c73c3
Sipepper <valera1gribnik@gmail.com>
2026-02-08 14:06:47 +0200

    removed unnecessary files

--- assets/snippets/all.lua
+++
@@ -1,5 +0,0 @@
-return {
-  s("fr", t("\\fraction{"),i(0),t("}{"),i(1),t("}")),
-  s("pma", { t("\\begin{pmatrix}"), i(0), t("\\end{pmatrix}") }),
-  s("pd", { t("\\frac{\\partial "),i(0),t("}{\\partial "),i(1),t("}") }),
-}
